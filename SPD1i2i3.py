72f8231e5ddd85654476a8857d0e18d121322410
Wirgiliusz <kamx98@gmail.com>
2020-03-27 15:36:11 +0100

    WORKING?!

--- SPD1i2i3.py
+++ SPD1i2i3.py
@@ -7,7 +7,7 @@ import copy
 #1. Dodac Carliera
 #2. Zrobic pomiary
 
-plik = open("data/data100.txt", "r")
+plik = open("data/data500.txt", "r")
 linie = plik.readlines()
 n = int(linie[0].split()[0])
 
@@ -230,9 +230,10 @@ def carlier(zad):
     idx_max = 0
     for j in range(0, len(pi)):
         C = calculate_Cmax_toidx(pi,j) + pi[j][2]
-        if C >= Cmax:
-            Cmax = C
+        if C == U:
+            #Cmax = C
             idx_max = j
+            #break
     b = pi[idx_max]
     # szukanie a
     Cmax = 0
@@ -243,9 +244,10 @@ def carlier(zad):
         for k in range(j, idx_max+1):
             p_sum += pi[k][1]
         C = pi[j][0] + b[2] + p_sum
-        if C > Cmax:
-            Cmax = C
+        if C == U:
+            #Cmax = C
             idx_min = j
+            break
     a = pi[idx_min]
     # szukanie c
     c = None
@@ -253,6 +255,7 @@ def carlier(zad):
     for j in range(idx_min, idx_max):
         if pi[j][2] < b[2]:
             idx_c = j
+            #break
     if idx_c != -1:
         c = pi[idx_c]
     else:
